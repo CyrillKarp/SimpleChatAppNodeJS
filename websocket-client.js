27a621566dc742b834e04980fb6e1a7f2aa44539
Cyrill <torreskirill@yandex.ru>
2018-06-04 00:17:19 +0300

    Broadcasting

--- websocket-client.js
+++ websocket-client.js
@@ -1,4 +1,9 @@
 const WS = new WebSocket('ws://localhost:3232');
+WS.onmessage = (payload) => {
+    console.log(payload.data);
+    
+    
+};
 
 document.forms[0].onsubmit = () => {
     let input = document.getElementById('message');
--- websocket-server.js
+++ websocket-server.js
@@ -4,6 +4,11 @@ const WSS = new WebSocketServer({port: 3232});
 WSS.on('connection', (ws) => {
     
     ws.on('message', (message) => {
+        
+        WSS.clients.forEach((client) =>{
+            client.send(message);
+        });
+        
         console.log(message);
     });
     
