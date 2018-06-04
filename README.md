dee0a206af244ee87665bbaa8abbcb8846dcaeb4
Cyrill <torreskirill@yandex.ru>
2018-06-04 23:43:38 +0300

    finish

---
+++ README.md
@@ -0,0 +1,5 @@
+# Super simple chat app 
+
+It works on node.js.<br>
+
+Thanks to Edwin Diaz for an idea and good node.js course! 
--- websocket-server.js
+++ websocket-server.js
@@ -1,18 +1,23 @@
 const WebSocketServer = require('ws').Server;
-const WSS = new WebSocketServer({port: 3232});
+const WSS = new WebSocketServer({
+    port: 3232
+});
 
 WSS.on('connection', (ws) => {
-    
+
     ws.on('message', (message) => {
-        
-        WSS.clients.forEach((client) =>{
-            client.send(message);
-        });
-        
+
+        if (message === 'close') {
+            ws.close();
+        } else {
+            WSS.clients.forEach((client) => {
+                client.send(message);
+            });
+        }
+
         console.log(message);
     });
-    
-    
+
     console.log("We are connected!")
-    
+
 });
